1709ed4d5915ee377cf68884c471c5434d72ee8b
SaiRedSoft48 <sai.prasad@redsoftware.in>
2025-02-24 12:57:51 +0000

    feat && refactor: Added and Customized doctype

--- task_manager/task_management/doctype/project/project.py
+++ task_manager/task_management/doctype/project/project.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2025, sai and contributors
 # For license information, please see license.txt
 
-# import frappe
+#import frappe
 from frappe.model.document import Document
 
 
 class Project(Document):
-	pass
+	pass																																																																																																																																																																																																											
---
+++ task_manager/task_management/doctype/team_management/team_management.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sai and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TeamManagement(Document):
+	pass
